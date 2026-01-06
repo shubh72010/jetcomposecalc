d0506d952a8e384d072945dd06e9cbbf4d2dbe5d
shubh72010 <108581630+shubh72010@users.noreply.github.com>
2026-01-06 18:43:20 +0530

    Initial commit: build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("com.google.dagger.hilt.android") version "2.47" apply false
+}
+
+task("clean") {
+    delete("build")
+}
